6a8d21dd362fce9c20aee580e05caf966e77ca7e
001007081 <ziyang.wang@student.tafesa.edu.au>
2023-11-20 13:23:04 +1030

    Added MainMenu design and logo

---
+++ UniversalCalculator/MainMenu.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Calculator
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public sealed partial class MainMenu : Page
+	{
+		public MainMenu()
+		{
+			this.InitializeComponent();
+		}
+
+		// Navigate to Math Calculator Application
+		private void mathCalculatorButton_Click(object sender, RoutedEventArgs e)
+		{
+			if (this.Frame != null)
+			{
+				this.Frame.Navigate(typeof(MainPage));
+			}
+		}
+
+		// Navigate to Mortgage Calculator Application
+		private void mortgageCalculatorButton_Click(object sender, RoutedEventArgs e)
+		{
+			/* Uncomment following code after created MortgageCalculator xaml page
+			 * if (this.Frame != null)
+			{
+				this.Frame.Navigate(typeof(MortgageCalculator));
+			}
+			*/
+		}
+
+		// Navigate to Currency Calculator Application
+		private void currencyCalculatorButton_Click(object sender, RoutedEventArgs e)
+		{
+			/* Uncomment following code after created CurrencyConverter xaml page
+			 * if (this.Frame != null)
+			{
+				this.Frame.Navigate(typeof(CurrencyConverter));
+			}
+			*/
+		}
+
+		// Exit the Application
+		private void exitButton_Click(object sender, RoutedEventArgs e)
+		{
+			System.Environment.Exit(0);
+		}
+	}
+}
